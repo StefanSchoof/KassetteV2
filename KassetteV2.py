bf40ce72a70551709792bf19bde7c371dc1c7a22
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2017-07-14 14:32:06 +0200

    use track request for better ai integration

--- KassetteV2.py
+++ KassetteV2.py
@@ -1,6 +1,7 @@
 from applicationinsights import TelemetryClient, exceptions
 import soco
 import os
+from datetime import datetime
 tc = TelemetryClient(os.environ['APPLICATIONINSIGHTS'])
 exceptions.enable(os.environ['APPLICATIONINSIGHTS'])
 
@@ -11,14 +12,22 @@ while (nfcNumber != 'exit'):
     nfcNumber = input('Ready for NFC input: ')
     if (nfcNumber != 'exit'):
         try:
+            start_time = datetime.utcnow()
             sonos = {s for s in soco.discover() if s.player_name == os.environ['SPEAKER']}.pop()
             playList = next(p for p in sonos.music_library.get_music_library_information('sonos_playlists') if p.title.endswith(nfcNumber))
             sonos.unjoin()
             sonos.clear_queue()
             sonos.add_to_queue(playList)
             sonos.play()
-            tc.track_event("Play", {'nfcNumer': nfcNumber, 'playList': playList.title })
+            duration = (datetime.utcnow() - start_time).total_seconds() * 1000
+            tc.track_request("Play", nfcNumber, True, duration = duration, properties = { 'playList': playList.title })
         except StopIteration:
-            tc.track_event('found no Playlist', {'nfcNumer': nfcNumber})
+            duration = (datetime.utcnow() - start_time).total_seconds() * 1000
+            tc.track_request("Play", nfcNumber, True, duration = duration, response_code = 404)
             print('found no Playlist for "' + nfcNumber + '"')
-        tc.flush()
+        except:
+            duration = (datetime.utcnow() - start_time).total_seconds() * 1000
+            tc.track_request("Play", nfcNumber, False, duration = duration, response_code = 500)
+            raise
+        finally:
+            tc.flush()
