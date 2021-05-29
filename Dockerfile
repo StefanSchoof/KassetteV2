091601898890b5e1937804bba99f98176b3d6c79
StefanSchoof <4662023+StefanSchoof@users.noreply.github.com>
2021-05-29 13:47:25 +0200

    use evdev to get input

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,9 @@
-#x86only FROM debian:testing as qemu
-
-#x86only RUN apt-get update && apt-get install -y \
-#x86only     qemu-user-static \
-#x86only  && rm -rf /var/lib/apt/lists/*
-
-FROM python:3.4
-
-#x86only COPY --from=qemu /usr/bin/qemu-arm-static /usr/bin
+FROM python:3.9
 
 WORKDIR /usr/src/app
 
 COPY requirements.txt patch.txt ./
-RUN pip install --no-cache-dir -r requirements.txt \
- && patch -d/usr/local/lib/python3.4/site-packages/soco < patch.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
--- KassetteV2.py
+++ KassetteV2.py
@@ -1,43 +1,59 @@
-"""A script to controll a sonos speaker with a NFC Card"""
 import os
-from datetime import datetime
-from applicationinsights import TelemetryClient, exceptions
 import soco
-exceptions.enable(os.environ['APPLICATIONINSIGHTS'])
+from evdev import InputDevice, categorize, ecodes
+
+
+def play(nfc_number):
+    print('start play "' + nfc_number + '"')
+    try:
+        sonos = {
+            s for s in soco.discover() if s.player_name == os.environ["SPEAKER"]
+        }.pop()
+        playlist = next(
+            p
+            for p in sonos.music_library.get_music_library_information(
+                "sonos_playlists"
+            )
+            if p.title.endswith(nfc_number)
+        )
+        sonos.unjoin()
+        sonos.clear_queue()
+        sonos.add_to_queue(playlist)
+        sonos.play_from_queue(0)
+    except StopIteration:
+        print('found no Playlist for "' + nfc_number + '"')
+
+
+scancodes = {
+    2: u"1",
+    3: u"2",
+    4: u"3",
+    5: u"4",
+    6: u"5",
+    7: u"6",
+    8: u"7",
+    9: u"8",
+    10: u"9",
+    11: u"0",
+}
+
 
 def main():
-    """runs input loop of kassette V2"""
-    telemetry_client = TelemetryClient(os.environ['APPLICATIONINSIGHTS'])
-    telemetry_client.track_event("Startup")
-    telemetry_client.flush()
-    nfc_number = ''
-    while nfc_number != 'exit':
-        nfc_number = input('Ready for NFC input: ')
-        if nfc_number != 'exit':
-            try:
-                start_time = datetime.utcnow()
-                sonos = {s for s in soco.discover() if s.player_name == os.environ['SPEAKER']}.pop()
-                playlist = next(p for p in
-                                sonos.music_library.get_music_library_information('sonos_playlists')
-                                if p.title.endswith(nfc_number))
-                sonos.unjoin()
-                sonos.clear_queue()
-                sonos.add_to_queue(playlist)
-                sonos.play_from_queue(0)
-                duration = (datetime.utcnow() - start_time).total_seconds() * 1000
-                telemetry_client.track_request("Play", nfc_number, True, duration=duration,
-                                               properties={'playList': playlist.title})
-            except StopIteration:
-                duration = (datetime.utcnow() - start_time).total_seconds() * 1000
-                telemetry_client.track_request("Play", nfc_number, True,
-                                               duration=duration, response_code=404)
-                print('found no Playlist for "' + nfc_number + '"')
-            except:
-                duration = (datetime.utcnow() - start_time).total_seconds() * 1000
-                telemetry_client.track_request("Play", nfc_number, False,
-                                               duration=duration, response_code=500)
-                raise
-            finally:
-                telemetry_client.flush()
+    accumluator = ""
+    devicePath = "/dev/input/event0"
+    dev = InputDevice()
+    dev.grab()
+
+    print("Start listining on " + devicePath)
+    for event in dev.read_loop():
+        if event.type == ecodes.EV_KEY:
+            data = categorize(event)
+            if data.keystate == 1:
+                if data.scancode == 28:
+                    play(accumluator)
+                    accumluator = ""
+                else:
+                    accumluator += scancodes.get(data.scancode)
+
 
 main()
